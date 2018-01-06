536b6352c3d0595414662760a235e69c76349eb1
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-01-06 17:27:01 +0530

    Random Number Generator
    Adding a project/ruby code to generate random number and retrieve results in a text file

---
+++ random_number_generator.rb
@@ -0,0 +1,16 @@
+
+
+# Generates a random number
+def generate_random_number
+	rand(100)
+end
+
+# Stores a number in the file results.yaml
+def store_results_file
+	aFile = File.new("./random_number_result.txt", "r+")
+	aFile.truncate(0)
+	number = generate_random_number
+	aFile.syswrite("#{number}") if aFile
+end
+
+store_results_file
